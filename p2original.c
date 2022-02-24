02fa4a1306fa65edf8ce7bb2aef58be2bfaa2ee0
kush46 <97089328+kushal46@users.noreply.github.com>
2022-02-24 10:05:33 +0000

    0

--- p2original.c
+++ p2original.c
@@ -1,24 +1,29 @@
 #include<stdio.h>
 #include<math.h>
 
-void input_triangle(float*x1,float*y1,float*x2,float*y2,float*x3,float*y3)
+void input_triangle(float*x1,float*y1)
 {
   printf("Enter the co-ordinates: \n");
-  scanf("%f%f%f%f%f%f",x1,y1,x2,y2,x3,y3);
+  scanf("%f%f",x1,y1);
 }
 
 int is_triangle(float x1,float y1,float x2,float y2,float x3,float y3)
-{ float s;
-  s=0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2));
-if(s=0)
-  return 0;
+
+{
+   int a,b,c;
+  a=sqrt(pow((x2-x1),2)+pow((y2-y1),2));
+  b=sqrt(pow((x3-x2),2)+pow((y3-y2),2));
+  c=sqrt(pow((x1-x3),2)+pow((y1-y3),2));
+  if((a+b)>c && (b+c)>a && (c+a)>b)
+    return 1;
   else
-  return 1;
+    return 0;
 }
+
 
 void output(int istriangle)
 {  
-  if (istriangle = 0)
+  if (istriangle == 0)
     printf("The given points doesn't form a triangle");
   else
     printf("The given points does  form a triangle");
@@ -27,7 +32,9 @@ void output(int istriangle)
 int main()
 { int istriangle;
   float x1,y1,x2,y2,x3,y3;
-  input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
+  input_triangle(&x1,&y1);
+  input_triangle(&x1,&y1);
+  input_triangle(&x1,&y1);
   istriangle=is_triangle(x1,y1,x2,y2,x3,y3);
   output(istriangle);
   return 0;
